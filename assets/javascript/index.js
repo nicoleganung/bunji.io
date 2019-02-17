87c011c0054d87668b1a8b6cf1d2658647dbba2a
nicole.ganung@outlook.com <iisixstIISIXST97>
2019-02-17 09:17:34 -0500

    First commit

---
+++ assets/javascript/index.js
@@ -0,0 +1,19 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+  	window.location.replace("main.html");
+
+  }
+});
+
+function login(){
+	var userEmail = document.getElementById("email_field").value;
+	var userPass = document.getElementById("password_field").value;
+
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+	
+  		window.alert("Error: " + errorMessage);
+	});
+}
